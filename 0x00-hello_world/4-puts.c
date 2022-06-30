3a503e80186e51ce605e81bf80aa20be49f393bb
abdullahim262 <abdullahim262@yahoo.com>
2022-06-30 14:02:19 -0700

    Puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * main - entry point
@@ -6,6 +5,7 @@
  * Return: 0 after printing the function
  */
 int main(void)
-{puts("\"programming is like building a multilingual puzzle");
-	return (0);
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
