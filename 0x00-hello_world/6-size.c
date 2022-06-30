4b9c71e1062687a69976d1b6cfa2f7290af9286c
abdullahim262 <abdullahim262@yahoo.com>
2022-06-30 16:24:42 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ long int b;
 long long int c;
 char d;
 float f;
-prinf("size of a char: %d byte(s)\n", sizeof(d));
-printf("size of an int: %d byte(s)\n", sizeof(a));
-printf("size of a long int: %d byte(s)\n", sizeof(b));
-printf("size of a long long int: %d byte(s)\n", sizeof(c));
-printf("size of a float: %d byte(s)\n", sizeof(f));
+prinf("size of a char: %d byte(s)\n", (long unsigned)sizeof(d));
+printf("size of an int: %a byte(s)\n", (long unsigned)sizeof(a));
+printf("size of a long int: %b byte(s)\n", (long unsigned)sizeof(b));
+printf("size of a long long int: %c byte(s)\n", (long unsigned)sizeof(c));
+printf("size of a float: %f byte(s)\n", (long unsigned)sizeof(f));
 return (0);
 }
