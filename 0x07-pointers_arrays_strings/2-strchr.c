f954ab376aa4729e78a96b234eceeedbeee46100
abdullahim262 <abdullahim262@yahoo.com>
2022-07-19 10:58:51 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,15 +8,17 @@
  */
 char *_strchr(char *s, char c)
 {
-	int a = 0, b;
-
-	while (s[a])
-		a++;
-	for (b = 0; b < a; b++)
+	while (*s != '\0') /* Declaring WHILE*/
 	{
-		if (c == s[b])
-			s += b;
+		if (*s == c) /*if s == c*/
+		{
+			return (s); /*return s*/
+		}
+		++s;
+	}
+	if (*s == c)
+	{
 		return (s);
 	}
-	return ('\0');
+	return (0);/* values null*/
 }
