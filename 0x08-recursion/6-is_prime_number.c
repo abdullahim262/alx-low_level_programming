b62f1ed17c1823947476970156b5be2e76ea8e57
abdullahim262 <abdullahim262@yahoo.com>
2022-07-20 14:42:00 -0700

    Task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * is_prime_number - check if number is prime or not.
+ * @n: number to be checked
+ *
+ * Return: 1 if number is prime
+ * 0 if number is not prime
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (helpfunction(n, 2));
+	}
+}
