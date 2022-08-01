6c72ef32b77c404bc930842175008e06ed23975a
abdullahim262 <abdullahim262@yahoo.com>
2022-08-01 10:05:49 -0700

    Task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
- * main - print the name of file program was compiled from
+ * main - print the name of file
+ *
  * Return: Always 0
  */
 int main(void)
